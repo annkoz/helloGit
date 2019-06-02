b9158dbf43475b1ec235b6915dc9ac7a1735481a
annkoz <Jkjkjif55>
2019-06-02 12:55:25 +0300

    Updated Hello file again in the test branch

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         int id = 2;
         public String text;
+        public String text2;
         System.out.println("bla-bla2" +id);
     }
 }
